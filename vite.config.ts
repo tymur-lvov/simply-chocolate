cc20376314c57911131e45f6988d64863a71677c
tymur-lvov <pmtl753@gmail.com>
2025-01-04 00:00:29 +0200

    chore: reset project

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vite';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-});
