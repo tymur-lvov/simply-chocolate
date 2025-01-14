261e776fe57ead9dffdfa581353815bfde668769
tymur-lvov <pmtl753@gmail.com>
2025-01-14 22:33:30 +0200

    chore: remove Icon wrapper

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,13 +1,9 @@
 import { icons } from '@assets';
 
-import { IconModule as css } from '@styles';
-
 export const Icon = ({ width, height, fragment }) => {
   return (
-    <div className={css.wrapper}>
-      <svg width={width} height={height}>
-        <use href={`${icons}#${fragment}`} />
-      </svg>
-    </div>
+    <svg width={width} height={height}>
+      <use href={`${icons}#${fragment}`} />
+    </svg>
   );
 };
