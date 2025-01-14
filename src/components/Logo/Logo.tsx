edd60cbd0303d6bd52371a0b845dcf7c8f41257b
tymur-lvov <pmtl753@gmail.com>
2025-01-14 23:08:37 +0200

    chore: add Logo Icon thumbnail

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,7 +7,9 @@ export const Logo = () => {
     <a href='/' className={css.text}>
       <div className={css.container}>
         <span className={css.accent}>SIMPLY</span>CHOCOLATE
-        <Icon width={21} height={21} fragment={'logo'} />
+        <div className={css.icon_container}>
+          <Icon width={21} height={21} fragment={'logo'} />
+        </div>
       </div>
     </a>
   );
