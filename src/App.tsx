56eb8245192c0dcee2a892f372fb08a5bccb8a4a
tymur-lvov <pmtl753@gmail.com>
2025-01-16 11:18:58 +0200

    chore: add Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Header } from '@components';
+import { Header, Main } from '@components';
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as MenuButtonModule } from './MenuButton/MenuButton.module.css';
 export * from './MenuButton/MenuButton.tsx';
+export * from './Main/Main.tsx';
 export { default as LogoModule } from './Logo/Logo.module.css';
 export * from './Logo/Logo.tsx';
 export * from './Icon/Icon.tsx';
