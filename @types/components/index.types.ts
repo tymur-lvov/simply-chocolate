04699bd2f69effd731c592eff85b43170dc1222a
tymur-lvov <pmtl753@gmail.com>
2025-01-17 13:19:19 +0200

    chore: add prop for class names

--- @types/components/index.types.ts
+++ @types/components/index.types.ts
@@ -1,9 +1,18 @@
 import type { FunctionComponent } from 'react';
 
 export interface IIconProps {
+  className: string;
   width: number;
   height: number;
+  fill?: string;
   fragment: string;
 }
 
+export interface IButtonProps {
+  className: string;
+  text?: string;
+}
+
 export type IIcon = FunctionComponent<IIconProps>;
+
+export type IButton = FunctionComponent<IButtonProps>;
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,9 +2,9 @@ import { icons } from '@assets';
 
 import type { IIcon } from '@types';
 
-export const Icon: IIcon = ({ width, height, fragment }) => {
+export const Icon: IIcon = ({ className, width, height, fill = 'none', fragment }) => {
   return (
-    <svg width={width} height={height}>
+    <svg className={className} width={width} height={height} fill={fill}>
       <use href={`${icons}#${fragment}`} />
     </svg>
   );
