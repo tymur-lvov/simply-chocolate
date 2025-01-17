44707201f5343cff98e0aed2947e59308dfc5259
tymur-lvov <pmtl753@gmail.com>
2025-01-17 13:24:24 +0200

    chore: elaborate Hero component add title, main buttons, aux buttons

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,51 @@
 import { container } from '@constants';
 
+import { Button, Icon } from '@components';
+
 import { HeroModule as css } from '@styles';
 
 export const Hero = () => {
   return (
     <div className={container}>
-      <section className={css.hero}>
-        <div className={css.hero__container}>
-          <h1 className={css.hero__title}>
+      <section className={css['hero']}>
+        <div className={css['hero__container']}>
+          <h1 className={css['hero__container__title']}>
             Treat yourself or a loved one to our finest ingredients for a moment of pure delight!
           </h1>
-          <div className={css.hero__main_buttons_container}></div>
+          <div className={css['hero__container__main-buttons-container']}>
+            <Button
+              className={css['hero__container__main-buttons-container__order-btn']}
+              text='Buy now'
+            />
+            <Button
+              className={css['hero__container__main-buttons-container__info-btn']}
+              text='How it&#x2019;s made'
+            />
+          </div>
+          <a className={css['hero__container__aux-buttons-link']} href='#'>
+            <div className={css['hero__container__aux-buttons-link__aux-buttons-container']}>
+              <p className={css['hero__container__aux-buttons-link__aux-buttons-container__text']}>
+                scroll down
+              </p>
+              <div
+                className={
+                  css['hero__container__aux-buttons-link__aux-buttons-container__icon-thumb']
+                }
+              >
+                <Icon
+                  className={
+                    css[
+                      'hero__container__aux-buttons-link__aux-buttons-container__icon-thumb__icon'
+                    ]
+                  }
+                  width={16}
+                  height={16}
+                  fragment='arrow_down'
+                  fill='var(--orange)'
+                />
+              </div>
+            </div>
+          </a>
         </div>
       </section>
     </div>
