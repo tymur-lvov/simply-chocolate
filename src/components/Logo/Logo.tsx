8545a512b596aea994a2a5477156f6a0bf5cefb2
tymur-lvov <pmtl753@gmail.com>
2025-01-20 11:13:53 +0200

    chore: adapt Logo component for desktop

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,12 +6,12 @@ import { LogoModule as css } from '@styles';
 
 export const Logo = () => {
   return (
-    <a href='/' className={css['logo__link']}>
+    <a className={css['logo__link']} href='/'>
       <div className={css['logo__wrapper']}>
         <span className={css['logo__accent']}>{data.title.accentText}</span>
         {data.title.mainText}
         <div className={css['logo__icon-thumb']}>
-          <Icon className={'logo__icon'} width={21} height={21} fragment={'logo'} />
+          <Icon className={css['logo__icon']} width={21} height={21} fragment={'logo'} />
         </div>
       </div>
     </a>
