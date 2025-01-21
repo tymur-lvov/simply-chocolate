8b3133670ffddb8a5126beb5324e9c3f0c19ccd9
tymur-lvov <pmtl753@gmail.com>
2025-01-21 15:53:03 +0200

    chore: add adaptive re-render

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
+
 import { Header, Main } from '@components';
 
 export const App = () => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  const resizeHandle = () => {
+    setViewportWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeHandle);
+
+    return () => window.removeEventListener('resize', resizeHandle);
+  }, []);
+
   return (
-    <>
+    <div key={viewportWidth}>
       <Header />
       <Main />
-    </>
+    </div>
   );
 };
