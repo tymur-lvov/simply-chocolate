9af03ab8091eeb3c5c2728440cfec33649e9041f
tymur-lvov <pmtl753@gmail.com>
2025-02-09 00:06:57 +0200

    chore: update Benefits component styles

--- src/components/BenefitsItem/BenefitsItem.tsx
+++ src/components/BenefitsItem/BenefitsItem.tsx
@@ -8,10 +8,8 @@ export const BenefitsItem: IBenefitsItem = ({ data: { title, description, icon }
   return (
     <>
       <Icon className={css.benefits_icon} data={icon} />
-      <div className={css.benefits_text_wrapper}>
-        <h3 className={css.benefits_sub_title}>{title}</h3>
-        <p className={css.benefits_description}>{description}</p>
-      </div>
+      <h3 className={css.benefits_sub_title}>{title}</h3>
+      <p className={css.benefits_description}>{description}</p>
     </>
   );
 };
