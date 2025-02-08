53b84457169e3873c053b3ccd9e72f800aa79eab
tymur-lvov <pmtl753@gmail.com>
2025-02-08 19:32:04 +0200

    chore: add visually_hidden css rule

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export const MOBILE_MEDIA_QUERY = '(max-width: 767px)';
 export const TABLET_MEDIA_QUERY = '(min-width: 768px) and (max-width:1157px)';
 export const DESKTOP_MEDIA_QUERY = '(min-width: 1158px)';
 
+export const VISUALLY_HIDDEN = 'visually_hidden';
 export const CONTAINER = 'container';
 export const MENU = 'menu';
 export const HEADER = 'header';
