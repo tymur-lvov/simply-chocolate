3030cc18efc6369cbb92a12834fee3141cceea10
tymur-lvov <pmtl753@gmail.com>
2025-02-11 17:54:39 +0200

    chore: add IngredientsList component

---
+++ src/components/IngredientsList/IngredientsList.tsx
@@ -0,0 +1,17 @@
+import { IngredientsItem } from '@components';
+
+import { ingredientsModule as css } from '@styles';
+
+import type { IIngredientsList } from '@types';
+
+export const IngredientsList: IIngredientsList = ({ data: { ingredientsItems } }) => {
+  return (
+    <ul className={css.ingredients_list}>
+      {ingredientsItems.map((ingredientsItem) => (
+        <li className={css.ingredients_item} key={ingredientsItem.title}>
+          <IngredientsItem data={ingredientsItem} />
+        </li>
+      ))}
+    </ul>
+  );
+};
