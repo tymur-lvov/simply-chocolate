7c4306644ee8b8ede0f3aa5594a537504ca44932
tymur-lvov <pmtl753@gmail.com>
2025-02-12 00:35:02 +0200

    chore: update IngredientsItem component styles

--- src/components/IngredientsItem/IngredientsItem.tsx
+++ src/components/IngredientsItem/IngredientsItem.tsx
@@ -4,12 +4,14 @@ import { ingredientsItemModule as css } from '@styles';
 
 import type { IIngredientsItem } from '@types';
 
-export const IngredientsItem: IIngredientsItem = ({ data: { icon, image, title } }) => {
+export const IngredientsItem: IIngredientsItem = ({ data: { link, icon, image, title } }) => {
   return (
     <>
-      <Icon data={icon} />
-      <Image className={css.ingredients_image} data={image} />
-      <Title className={css.ingredients_title} data={title} />
+      <a className={css.ingredients_link} href={link.href}>
+        <Icon className={css.ingredients_icon} data={icon} />
+        <Image className={css.ingredients_image} data={image} />
+        <Title className={css.ingredients_title} data={title} />
+      </a>
     </>
   );
 };
