fc9d1ddc41528bf92ad18929aa6197a2626daf9b
tymur-lvov <pmtl753@gmail.com>
2025-02-14 18:21:52 +0200

    chore: add components css modules

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,4 +1,4 @@
-import { x1_QUERY, x2_QUERY } from '@constants';
+import { x1_MEDIA_QUERY, x2_MEDIA_QUERY } from '@constants';
 
 import type { IImage } from '@types';
 
@@ -6,8 +6,8 @@ export const Image: IImage = ({ className, data: { width, height, paths, alt } }
   return (
     <div className={className}>
       <picture>
-        <source media={x1_QUERY} srcSet={paths.x1} />
-        <source media={x2_QUERY} srcSet={paths.x2} />
+        <source media={x1_MEDIA_QUERY} srcSet={paths.x1} />
+        <source media={x2_MEDIA_QUERY} srcSet={paths.x2} />
         <img width={width} height={height} src={paths.x1} alt={alt} />
       </picture>
     </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 export const MOBILE_MEDIA_QUERY = '(max-width: 767px)';
 export const TABLET_MEDIA_QUERY = '(min-width: 768px) and (max-width:1157px)';
 export const DESKTOP_MEDIA_QUERY = '(min-width: 1158px)';
-export const x1_QUERY = '(max-resolution: 191dpi)';
-export const x2_QUERY = '(min-resolution: 192dpi)';
+export const x1_MEDIA_QUERY = '(max-resolution: 191dpi)';
+export const x2_MEDIA_QUERY = '(min-resolution: 192dpi)';
 
 export const VISUALLY_HIDDEN = 'visually_hidden';
 export const CONTAINER = 'container';
