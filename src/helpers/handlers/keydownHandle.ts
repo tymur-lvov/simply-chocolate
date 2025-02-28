0281d3da5ed85dfe8f60d97d9e0f5d3aed1ed4fe
tymur-lvov <pmtl753@gmail.com>
2025-02-28 13:26:41 +0200

    refactor: restructure Input component

--- src/helpers/handlers/keydownHandle.ts
+++ src/helpers/handlers/keydownHandle.ts
@@ -1,9 +1,10 @@
-import { toggleModal } from '@helpers';
+import { toggleModal, toggleScrollLock } from '@helpers';
 
 import type { IKeydownHandle } from '@types';
 
 export const keydownHandle: IKeydownHandle = (event, setIsModalOpen) => {
   if (event.key === 'Escape') {
     toggleModal('close', setIsModalOpen);
+    toggleScrollLock();
   }
 };
--- src/helpers/handlers/reviewsButtonClickHandle.ts
+++ src/helpers/handlers/reviewsButtonClickHandle.ts
@@ -1,7 +1,8 @@
-import { toggleModal } from '@helpers';
+import { toggleModal, toggleScrollLock } from '@helpers';
 
 import type { IReviewsButtonClickHandle } from '@types';
 
 export const reviewsButtonClickHandle: IReviewsButtonClickHandle = (setIsModalOpen) => {
   toggleModal('open', setIsModalOpen);
+  toggleScrollLock();
 };
