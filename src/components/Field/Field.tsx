3ec000bd985fd401f319b0777d3968f080bc2da4
tymur-lvov <pmtl753@gmail.com>
2025-03-05 16:33:03 +0200

    chore: add review submit form validation

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,17 +1,35 @@
+import { clsx } from 'clsx';
+
 import { Checkbox, Input, Label, TextArea } from '@components';
 
 import { fieldModule as css } from '@styles';
 
 import type { IField } from '@types';
 
-export const Field: IField = ({ data }) => {
+export const Field: IField = ({ isFieldDataValid, data, onChange }) => {
   return (
     <>
       {data.label && <Label data={data} />}
       {(data.type === 'text' || data.type === 'email' || data.type === 'number') && (
-        <Input className={css.review_form_input} data={data} />
+        <Input
+          className={clsx({
+            [css.review_form_input]: true,
+            [css.review_form_input__error]: isFieldDataValid,
+          })}
+          data={data}
+          onChange={onChange}
+        />
       )}
-      {data.type === 'textArea' && <TextArea className={css.review_form_input} data={data} />}
+      {data.type === 'textArea' && (
+        <TextArea
+          className={clsx({
+            [css.review_form_input]: true,
+            [css.review_form_input__error]: isFieldDataValid,
+          })}
+          data={data}
+          onChange={onChange}
+        />
+      )}
       {data.type === 'checkbox' && <Checkbox data={data} />}
     </>
   );
