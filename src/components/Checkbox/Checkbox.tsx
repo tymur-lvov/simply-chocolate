05c2be9f71d3b154b5d00b0bc17262dc2d329ecc
tymur-lvov <pmtl753@gmail.com>
2025-03-09 17:15:08 +0200

    fix: update Field component props

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -4,11 +4,7 @@ import { checkboxModule as css } from '@styles';
 
 import type { ICheckbox } from '@types';
 
-export const Checkbox: ICheckbox = ({
-  data: { id, type, icon, description, link },
-  isFieldValid,
-  onChange,
-}) => {
+export const Checkbox: ICheckbox = ({ data: { id, type, icon, description, link }, onChange }) => {
   return (
     <div className={css.review_form_checkbox_description_wrapper}>
       <div className={css.review_form_checkbox_wrapper}>
