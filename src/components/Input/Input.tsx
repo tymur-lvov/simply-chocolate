9bf60ccf5aac34117ece3379485915010888ec4c
tymur-lvov <pmtl753@gmail.com>
2025-03-10 17:16:30 +0200

    chore: update review submit form validation

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -6,14 +6,20 @@ import { inputModule as css } from '@styles';
 
 import type { IInput } from '@types';
 
-export const Input: IInput = ({ className, data: { id, type, icon }, isFieldValid, onChange }) => {
+export const Input: IInput = ({
+  className,
+  data: { id, type, icon },
+  isFieldValid,
+  isSubmitAttempted,
+  onChange,
+}) => {
   return (
     <div className={css.review_form_input_icon_wrapper}>
       <input className={className} id={id} type={type} autoComplete='true' onChange={onChange} />
       <Icon
         className={clsx({
           [css.review_form_input_icon]: true,
-          [css.review_form_input_icon__error]: !isFieldValid,
+          [css.review_form_input_icon__error]: !isFieldValid && isSubmitAttempted,
         })}
         data={icon}
       />
--- src/components/ReviewSubmitForm/ReviewSubmitForm.tsx
+++ src/components/ReviewSubmitForm/ReviewSubmitForm.tsx
@@ -6,26 +6,54 @@ import { SECTION_TITLE, SECTION_TITLE_ACCENT } from '@constants';
 
 import { reviewSubmitFormModule as css } from '@styles';
 
-import type { FormEvent } from 'react';
+import type { FormEvent, MouseEvent } from 'react';
 import type { IReviewSubmitForm } from '@types';
 
 export const ReviewSubmitForm: IReviewSubmitForm = ({ data: { title, inputs, button } }) => {
-  const [_, setIsSubmitAttempted] = useState(false);
+  const [formStatus, setFormStatus] = useState({
+    isSubmitAttempted: false,
+  });
+
+  const [errorPopupStatus, setErrorPopupStatus] = useState<{
+    isErrorPopupVisible: boolean;
+    errorPopupIndex: number | null | undefined;
+  }>({
+    isErrorPopupVisible: false,
+    errorPopupIndex: null,
+  });
 
   const formSubmitHandle = (event: FormEvent) => {
     event.preventDefault();
 
-    setIsSubmitAttempted(true);
+    setFormStatus((prev) => ({ ...prev, isSubmitAttempted: true }));
+  };
+
+  const formClickHandle = (event: MouseEvent) => {
+    const element = event.target as HTMLElement;
+
+    if (element.nodeName !== 'svg' && element.nodeName !== 'use') {
+      setErrorPopupStatus({
+        isErrorPopupVisible: false,
+        errorPopupIndex: null,
+      });
+    }
   };
 
   return (
-    <form className={css.review_form} onSubmit={formSubmitHandle}>
+    <form className={css.review_form} onSubmit={formSubmitHandle} onClick={formClickHandle}>
       <Title
         classNames={[SECTION_TITLE, SECTION_TITLE_ACCENT, css.review_form_title]}
         data={title}
       />
-      {inputs.map((input) => (
-        <Field data={input} key={input.id} />
+      {inputs.map((input, index) => (
+        <Field
+          data={input}
+          index={index}
+          key={input.id}
+          formStatus={formStatus}
+          errorPopupStatus={errorPopupStatus}
+          setErrorPopupStatus={setErrorPopupStatus}
+        />
       ))}
       <Button className={css.review_form_button} type='submit' data={button} />
     </form>
