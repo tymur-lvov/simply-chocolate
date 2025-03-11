23affd3e6fd1577cd57f7d3d7c30d0463ff2149b
tymur-lvov <pmtl753@gmail.com>
2025-03-11 19:28:14 +0200

    chore: update review submit form validation

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -4,12 +4,10 @@ import { errorMessageModule as css } from '@styles';
 
 import type { IErrorMessage } from '@types';
 
-export const ErrorMessage: IErrorMessage = ({ className, onClick }) => {
+export const ErrorMessage: IErrorMessage = ({ className, data, onClick }) => {
   return (
     <div className={clsx(css.error_message_text_wrapper, className)} onClick={onClick}>
-      <p className={css.error_message_text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, labore?
-      </p>
+      <p className={css.error_message_text}>{data?.text}</p>
     </div>
   );
 };
