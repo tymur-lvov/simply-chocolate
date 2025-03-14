570d6fd755e6912ffc89df1e5ed7492627e62f43
tymur-lvov <pmtl753@gmail.com>
2025-03-14 16:31:10 +0200

    chore: update review submit form validation

--- src/components/ReviewSubmitForm/ReviewSubmitForm.tsx
+++ src/components/ReviewSubmitForm/ReviewSubmitForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Field, Title } from '@components';
+import { Button, Field, Title } from '@components';
 
 import { SECTION_TITLE, SECTION_TITLE_ACCENT } from '@constants';
 
@@ -9,7 +9,7 @@ import { reviewSubmitFormModule as css } from '@styles';
 import type { FormEvent, MouseEvent } from 'react';
 import type { IReviewSubmitForm } from '@types';
 
-export const ReviewSubmitForm: IReviewSubmitForm = ({ data: { title, inputs } }) => {
+export const ReviewSubmitForm: IReviewSubmitForm = ({ data: { title, inputs, button } }) => {
   const [formEventStatus, setFormEventStatus] = useState({
     isSubmitAttempted: false,
     isAnyFieldChanged: false,
@@ -80,7 +80,7 @@ export const ReviewSubmitForm: IReviewSubmitForm = ({ data: { title, inputs } })
           setFormErrorStatus={setFormErrorStatus}
         />
       ))}
-      {/* <Button className={css.review_form_button} type='submit' data={button} /> */}
+      <Button className={css.review_form_button} type='submit' data={button} />
     </form>
   );
 };
