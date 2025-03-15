81bb2ddfb08fe7d7e6b64c4f32f71ac168251a5a
tymur-lvov <pmtl753@gmail.com>
2025-03-15 17:35:09 +0200

    refactor: restructure ReviewSubmitForm component

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,73 +1,20 @@
 import { clsx } from 'clsx';
 import { useState } from 'react';
 
+import { reviewFieldChangeHandle, reviewFieldErrorIconClickHandle } from '@helpers';
+
 import { Checkbox, ErrorMessage, Icon, Input, Label, TextArea } from '@components';
 
 import { fieldModule as css } from '@styles';
 
-import type { ChangeEvent } from 'react';
 import type { IField } from '@types';
 
-export const Field: IField = ({
-  data,
-  index,
-  formEventStatus,
-  formErrorStatus,
-  setFormEventStatus,
-  setFormErrorStatus,
-}) => {
+export const Field: IField = ({ data, fieldIndex, reviewFormStatus, setReviewFormStatus }) => {
   const [isFieldValid, setIsFieldValid] = useState(false);
 
-  const isSubmitAttempted = formEventStatus?.isSubmitAttempted;
+  const { isSubmitAttempted } = reviewFormStatus.event;
 
-  const isErrorPopupVisible = formErrorStatus?.isErrorPopupVisible;
-
-  const errorFieldIndex = formErrorStatus?.errorFieldIndex;
-
-  const validateField = (field: HTMLInputElement) => {
-    const fieldName = field.id as keyof typeof userInputRegExpCollection | 'privacy';
-
-    const userInput = field.value;
-
-    const isCheckboxChecked = field.checked;
-
-    if (fieldName === 'privacy') {
-      return isCheckboxChecked;
-    }
-
-    const userInputRegExpCollection = {
-      name: /^[A-Za-z\s\-]{2,50}$/,
-      email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      phone: /^(\+?\d{1,3})?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{2}[-.\s]?\d{2}$/,
-      comment: /^[A-Za-z0-9\s.,!?()\-'"%$#@:;]{2,500}$/,
-    };
-
-    return userInputRegExpCollection[fieldName].test(userInput);
-  };
-
-  const fieldChangeHandle = (event: ChangeEvent) => {
-    const field = event.target as HTMLInputElement;
-
-    const errorKey = `is${field.id}Error`;
-
-    const isFieldValid = validateField(field);
-
-    setFormEventStatus?.((prev) => ({ ...prev, isAnyFieldChanged: true }));
-
-    setFormErrorStatus?.((prev) => ({ ...prev, [errorKey]: isFieldValid }));
-
-    setIsFieldValid(isFieldValid);
-  };
-
-  const errorIconClickHandle = () => {
-    if (isSubmitAttempted && !isFieldValid) {
-      setFormErrorStatus?.((prev) => ({
-        ...prev,
-        isErrorPopupVisible: !prev.isErrorPopupVisible,
-        errorFieldIndex: index,
-      }));
-    }
-  };
+  const { isErrorPopupVisible, errorFieldIndex } = reviewFormStatus.error;
 
   return (
     <>
@@ -82,8 +29,11 @@ export const Field: IField = ({
               })}
               data={data}
               isFieldValid={isFieldValid}
-              isSubmitAttempted={isSubmitAttempted}
-              onChange={fieldChangeHandle}
+              reviewFormStatus={reviewFormStatus}
+              setReviewFormStatus={setReviewFormStatus}
+              onChange={(event) =>
+                reviewFieldChangeHandle(event, setReviewFormStatus, setIsFieldValid)
+              }
             />
           )}
           {data.type === 'textArea' && (
@@ -93,7 +43,9 @@ export const Field: IField = ({
                 [css.review_form_field__error]: !isFieldValid && isSubmitAttempted,
               })}
               data={data}
-              onChange={fieldChangeHandle}
+              onChange={(event) =>
+                reviewFieldChangeHandle(event, setReviewFormStatus, setIsFieldValid)
+              }
             />
           )}
           <Icon
@@ -103,12 +55,19 @@ export const Field: IField = ({
               [css.review_form_input_icon__error_icon__visible]: !isFieldValid && isSubmitAttempted,
             })}
             data={data.error?.icon}
-            onClick={errorIconClickHandle}
+            onClick={() =>
+              reviewFieldErrorIconClickHandle(
+                fieldIndex,
+                isFieldValid,
+                reviewFormStatus,
+                setReviewFormStatus
+              )
+            }
           />
           <ErrorMessage
             className={clsx({
               [css.error_message]: true,
-              [css.error_message__visible]: isErrorPopupVisible && errorFieldIndex === index,
+              [css.error_message__visible]: isErrorPopupVisible && errorFieldIndex === fieldIndex,
             })}
             data={data.error}
           />
@@ -118,8 +77,8 @@ export const Field: IField = ({
         <Checkbox
           data={data}
           isFieldValid={isFieldValid}
-          isSubmitAttempted={isSubmitAttempted}
-          onChange={fieldChangeHandle}
+          reviewFormStatus={reviewFormStatus}
+          onChange={(event) => reviewFieldChangeHandle(event, setReviewFormStatus, setIsFieldValid)}
         />
       )}
     </>
