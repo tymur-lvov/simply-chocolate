ad503fe15ad19f8f63e2c8f0639aa1e727fe27ac
tymur-lvov <pmtl753@gmail.com>
2025-03-17 15:47:37 +0200

    chore: update review submit form validation

--- src/helpers/handlers/backdropClickHandle.ts
+++ src/helpers/handlers/backdropClickHandle.ts
@@ -5,6 +5,7 @@ import type { IBackdropClickHandle } from '@types';
 export const backdropClickHandle: IBackdropClickHandle = (event, setIsModalOpen) => {
   if (event.target === event.currentTarget) {
     toggleModal('close', setIsModalOpen);
+
     toggleScrollLock();
   }
 };
