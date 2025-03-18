bc6714520f831f55ee442a9cb2f7f68a1dabf261
tymur-lvov <pmtl753@gmail.com>
2025-03-18 17:59:36 +0200

    chore: update review submit form validation

--- src/components/IngredientsItem/IngredientsItem.tsx
+++ src/components/IngredientsItem/IngredientsItem.tsx
@@ -1,17 +1,36 @@
-import { Icon, Image, Title } from '@components';
+import { useState } from 'react';
+
+import { ingredientsItemClickHandle } from '@helpers';
+
+import { Icon, Image, Modal, Portal, Title, IngredientDescription } from '@components';
 
 import { ingredientsItemModule as css } from '@styles';
 
 import type { IIngredientsItem } from '@types';
 
-export const IngredientsItem: IIngredientsItem = ({ data: { link, icon, image, title } }) => {
+export const IngredientsItem: IIngredientsItem = ({ data: { icon, image, title, modal } }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
-      <a className={css.ingredients_link} href={link.href}>
+      <div
+        className={css.ingredients_wrapper}
+        onClick={() => ingredientsItemClickHandle(setIsModalOpen)}
+      >
         <Icon className={css.ingredients_icon} data={icon} />
         <Image className={css.ingredients_image} data={image} />
         <Title className={css.ingredients_title} data={title} />
-      </a>
+      </div>
+      <Portal>
+        <Modal
+          data={modal}
+          variant='ingredientsInfo'
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        >
+          <IngredientDescription data={modal.ingredientDescription} />
+        </Modal>
+      </Portal>
     </>
   );
 };
