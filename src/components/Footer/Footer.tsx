d6d48ceea145aefd22ea1dbf663e0b41f6a10e2f
tymur-lvov <pmtl753@gmail.com>
2025-03-27 19:56:29 +0200

    chore: update Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,48 @@
+import { clsx } from 'clsx';
+
+import {
+  Title,
+  NavList,
+  SocialsList,
+  ContactsList,
+  SectionTitle,
+  SubscriptionForm,
+} from '@components';
+
+import { CONTAINER, MENU, SECTION_TITLE_ACCENT } from '@constants';
+
+import { footerModule as css } from '@styles';
+
 import type { IFooter } from '@types';
 
-export const Footer: IFooter = () => {
-  return <></>;
+export const Footer: IFooter = ({
+  data: {
+    navList,
+    socialsList,
+    contactsList,
+    socialsTitle,
+    sectionTitle,
+    contactsTitle,
+    subscriptionForm,
+  },
+}) => {
+  return (
+    <footer className={css.footer}>
+      <div className={CONTAINER}>
+        <SectionTitle classNames={[SECTION_TITLE_ACCENT, css.section_title]} data={sectionTitle} />
+        <div className={css.list_wrapper}>
+          <Title className={css.list_title} data={contactsTitle} />
+          <ContactsList data={contactsList} />
+        </div>
+        <div className={clsx(css.list_wrapper, css.list_wrapper__socials)}>
+          <Title className={css.list_title} data={socialsTitle} />
+          <SocialsList variant={MENU} data={socialsList} />
+        </div>
+        <div className={css.list_wrapper}>
+          <NavList variant={MENU} className={css.list_wrapper} data={navList} />
+        </div>
+        <SubscriptionForm data={subscriptionForm} />
+      </div>
+    </footer>
+  );
 };
