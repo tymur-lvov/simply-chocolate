957259d6f6809ec9b128deda4d6e5330b0ee2a78
tymur-lvov <pmtl753@gmail.com>
2025-03-29 19:30:16 +0200

    chore: update Footer component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,11 +2,24 @@ import { getIconPath } from '@helpers';
 
 import type { IIcon } from '@types';
 
-export const Icon: IIcon = ({ className, fragment, fragmentKey, data, onClick }) => {
+export const Icon: IIcon = ({
+  data,
+  fragment,
+  className,
+  fragmentKey,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+}) => {
   const iconPath = getIconPath(data?.fragment || fragment || '', fragmentKey, data?.fragments);
 
   return (
-    <div className={className} onClick={onClick}>
+    <div
+      className={className}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       <svg width={data?.width} height={data?.height}>
         <use href={iconPath} />
       </svg>
