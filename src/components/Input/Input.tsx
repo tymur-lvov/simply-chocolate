0ec55a1fbb5a66012de3a0007561b25516eb672a
tymur-lvov <pmtl753@gmail.com>
2025-03-31 18:38:06 +0300

    chore: update Footer component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,7 @@
 import { clsx } from 'clsx';
+import { useEffect } from 'react';
+
+import { getSavedValue } from '@helpers';
 
 import { Icon } from '@components';
 
@@ -10,14 +13,36 @@ export const Input: IInput = ({
   data: { id, type, icon },
   className,
   formStatus,
+  fieldValues,
   isFieldValid,
   onChange,
+  setFormStatus,
+  setFieldValues,
+  setIsFieldValid,
 }) => {
+  useEffect(() => {
+    const savedValue = getSavedValue(id);
+
+    if (savedValue) {
+      setFieldValues((prev) => ({ ...prev, [id]: savedValue }));
+    }
+  }, []);
+
   const { isSubmitAttempted } = formStatus;
 
   return (
     <div className={css.review_form_input_icon_wrapper}>
-      <input className={className} id={id} type={type} autoComplete='true' onChange={onChange} />
+      <input
+        className={className}
+        id={id}
+        type={type}
+        autoComplete='true'
+        value={fieldValues[id as keyof typeof fieldValues]}
+        onChange={(event) => {
+          setFieldValues((prev) => ({ ...prev, [id]: event.target.value }));
+          onChange(event, setFormStatus, setIsFieldValid);
+        }}
+      />
       <Icon
         className={clsx({
           [css.review_form_input_icon]: true,
