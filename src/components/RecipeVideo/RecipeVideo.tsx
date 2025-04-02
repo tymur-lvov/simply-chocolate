c5c1b4b66a41013c37d1ba6bdd0ac384b30bebc9
tymur-lvov <pmtl753@gmail.com>
2025-04-02 19:38:23 +0300

    chore: add embed video

---
+++ src/components/RecipeVideo/RecipeVideo.tsx
@@ -0,0 +1,41 @@
+import { clsx } from 'clsx';
+import { useEffect } from 'react';
+
+import {
+  keydownHandle,
+  backdropClickHandle,
+  addKeydownEventListener,
+  removeKeydownEventListener,
+} from '@helpers';
+
+import { recipeVideoModule as css } from '@styles';
+
+import type { IRecipeVideo } from '@types';
+
+export const RecipeVideo: IRecipeVideo = ({ isVideoOpen, setIsVideoOpen }) => {
+  useEffect(() => {
+    addKeydownEventListener(keydownHandle, setIsVideoOpen);
+
+    return () => removeKeydownEventListener(keydownHandle, setIsVideoOpen);
+  }, []);
+
+  return (
+    <div
+      className={clsx({
+        [css.video_backdrop]: true,
+        [css.video_backdrop__visible]: isVideoOpen,
+      })}
+      onClick={(event) => backdropClickHandle(event, setIsVideoOpen)}
+    >
+      <div className={css.video_wrapper}>
+        <iframe
+          width='922'
+          height='519'
+          src='https://www.youtube.com/embed/DISjdfkSjc8'
+          title='How It&#39;s Made: Chocolate'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        ></iframe>
+      </div>
+    </div>
+  );
+};
