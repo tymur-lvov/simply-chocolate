5ae559265f60481fa297aaff8abba5d63bf2b6b1
tymur-lvov <pmtl753@gmail.com>
2025-04-02 19:38:16 +0300

    chore: add embed video

--- src/components/HowItsMade/HowItsMade.tsx
+++ src/components/HowItsMade/HowItsMade.tsx
@@ -1,6 +1,9 @@
 import { clsx } from 'clsx';
+import { useState } from 'react';
 
-import { Icon, RecipeList, SectionTitle, Title } from '@components';
+import { recipeVideoClickHandle } from '@helpers';
+
+import { Icon, Portal, RecipeList, RecipeVideo, SectionTitle, Title } from '@components';
 
 import { CONTAINER, SECTION, SECTION_TITLE, SECTION_TITLE_ACCENT } from '@constants';
 
@@ -11,12 +14,18 @@ import type { IHowItsMade } from '@types';
 export const HowItsMade: IHowItsMade = ({
   data: { sectionTitle, videoThumb, title, recipeList },
 }) => {
+  const [isVideoOpen, setIsVideoOpen] = useState(false);
+
   return (
     <section className={clsx(css.how_its_made_section, SECTION)} id='recipe_section'>
       <div className={clsx(css.how_its_made_container, CONTAINER)}>
         <SectionTitle classNames={[SECTION_TITLE, SECTION_TITLE_ACCENT]} data={sectionTitle} />
         <div className={css.how_its_made_video_thumb_recipe_list_wrapper}>
-          <div className={css.how_its_made_video_thumb} tabIndex={0}>
+          <div
+            className={css.how_its_made_video_thumb}
+            tabIndex={0}
+            onClick={() => recipeVideoClickHandle(setIsVideoOpen)}
+          >
             <Icon className={css.how_its_made_video_thumb_icon} data={videoThumb.icon} />
           </div>
           <div className={css.how_its_made_recipe_list_wrapper}>
@@ -25,6 +34,11 @@ export const HowItsMade: IHowItsMade = ({
           </div>
         </div>
       </div>
+      {isVideoOpen && (
+        <Portal>
+          <RecipeVideo isVideoOpen={isVideoOpen} setIsVideoOpen={setIsVideoOpen} />
+        </Portal>
+      )}
     </section>
   );
 };
